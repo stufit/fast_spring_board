c55dab31942ff035ab3a365e96219210ed67b5c2
이관영 <71133204+stufit@users.noreply.github.com>
2023-10-10 09:37:38 +0900

    도메인 설계(2)

--- dashboard/src/main/java/fastcampus/dashboard/domain/Article.java
+++ dashboard/src/main/java/fastcampus/dashboard/domain/Article.java
@@ -3,12 +3,12 @@ package fastcampus.dashboard.domain;
 import java.time.LocalDateTime;
 
 public class Article {
-    private Long id;
-    private String title;
-    private String content;
-    private String hashtag;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private String createdBy;
-    private String updatedBy;
+    private Long id; //
+    private String title; // 제목
+    private String content; // 본문
+    private String hashtag; // 태그
+    private LocalDateTime createdAt; // 생성일시
+    private LocalDateTime updatedAt; // 수정일시
+    private String createdBy; // 생성자
+    private String updatedBy; // 수정자
 }
--- dashboard/src/main/java/fastcampus/dashboard/domain/ArticleComment.java
+++ dashboard/src/main/java/fastcampus/dashboard/domain/ArticleComment.java
@@ -4,11 +4,11 @@ import java.time.LocalDateTime;
 
 public class ArticleComment {
     private Long id;
-    private Article article;
-    private String content;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private String createdBy;
-    private String updatedBy;
+    private Article article; // 게시글id
+    private String content; // 본문
+    private LocalDateTime createdAt; // 생성일시
+    private LocalDateTime updatedAt; // 수정일시
+    private String createdBy; // 생성자
+    private String updatedBy; // 수정자
 
 }
